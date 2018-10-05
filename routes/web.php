f9fbf12cf2bc354c3445a3bc703c6370ec90678c
reinaldocelsos <42174545+reinaldocelsos@users.noreply.github.com>
2018-10-04 21:34:20 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware(['auth'])->prefix('admin')->namespace('Admin')->group(function(){
     Route::resource('artigos', 'ArtigosController');
+    Route::resource('usuarios', 'UsuariosController');
 });
